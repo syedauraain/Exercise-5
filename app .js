fc66a7688ad99dfae8afbc7dcc86579a6b0d9e16
syedauraain <160678874+syedauraain@users.noreply.github.com>
2024-04-18 15:29:26 -0700

    Did Favourite_Number

---
+++ app .js
@@ -0,0 +1,4 @@
+"use strict";
+const favorite_number = 6;
+const message = 'here is your favorite number ' + favorite_number;
+console.log(message);
---
+++ app .ts
@@ -0,0 +1,3 @@
+const favorite_number :number = 6;
+const message :string = 'here is your favorite number ' + favorite_number 
+console.log(message) ;
